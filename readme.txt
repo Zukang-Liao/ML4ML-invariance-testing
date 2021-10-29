af0167db8d7c2bf419d32ce2e72c34c5ad30f649
Zukang Liao <zukang.liao15@ic.ac.uk>
2021-10-29 12:09:26 +0100

    First commit

---
+++ readme.txt
@@ -0,0 +1,41 @@
+General guideline to run the code:
+
+(1). Standard CNN training (train.py)
+    A series of CNNs on either MNIST or CIFAR or other databases should be trained in advance. We use the code train.py to train many CNNs using the metadata specify in metadata.txt.
+    After the CNNs are trained, we use model id (mid) to record them.
+
+
+(2). Model database (model.py)
+    Our model database consists of 4 different partitions:
+	partition (a): mid: 1-100, t1-t50. VGG13bn trained on CIFAR for rotation testing.
+	partition (b): mid: 101-200, t101-t150. VGG13bn trained on CIFAR for brightness testing.
+	partition (c): mid: 201-300, t201-t250. VGG13bn trained on CIFAR for scaling testing.
+	partition (d): mid: 301-400, t301-t350. CNN5 trained on MNIST for rotation testing.
+	# Mid starting with "t" is a hold-out set. 
+	# When using three-fold cross validation, the hold-out set is always treated as one fold.
+	# While the rest 100 "regular" models are randomly split into two folds.
+    The assessment labels are provided in mlabel.txt. In this work, we focus on invariance quality.
+    Notes: Model 1 to 15 are trained on CPU, other models are trained on GPU.
+           Model 101 to 200, t101 to t150: preprocessing -- normalised to [0, 1], Others: [-0.5, 0.5]
+
+
+
+(3). Invariance testing data (save_invariance_results.py)
+    For a given CNN, please run: "python save_invariance_results.py --mid=mid --aug_type=r" where mid is the index of the CNN and aug_type: "r" for "rotation", "s" for "scaling" and "b" for "brightness".
+    The script will generate two .npy files, namely test_results1515.npy (CONF) and test_actoverall1515.npy (CONV).
+    For partition (d), please specify --dbname="mnist".
+
+
+(4). Variance matrices (matrices_CONF.py and matrices_CONV.py)
+    Run "python matrices_CONF.py --mid=mid --aug_type=r" to generate variance matrices (CONF).
+    Run "python matrices_CONV.py --mid=mid --aug_type=r" to generate variance matrices (CONV).
+
+
+(5). Measurements (measurements.py)
+    Run "python measurements.py --mid=mid --aug_type=r" to generate a json file consisting of all measurements for the model.
+    For partition (d), please specify --dbname="mnist".
+    
+
+(6). ML4ML assessors (ML4MLassessor.py)
+    Run "python ML4MLassessor.py --aug_type=r" to train an ML4ML assessor with different types of ml algorithms. And test the performance of the assessor on the testing set of the model-database.
+    For partition (d), please specify --dbname="mnist".
--- sampler.py
+++ sampler.py
@@ -1,3 +1,5 @@
+# functions that assist training CNN models
+
 import numpy as np
 from torch.utils.data import Sampler, RandomSampler
 import collections
@@ -18,7 +20,6 @@ class imbalanceSampler(Sampler):
         self.shuffle = shuffle
     
     def __iter__(self):
-        # return iter(self.chosen_indices)
         batch = []
         for idx in self.chosen_indices:
             batch.append(idx)
@@ -69,83 +70,3 @@ class orderedSampler(Sampler):
     
     def __len__(self):
         return self.nb_examples
-
-
-# Used for generating images from one class only
-class mySampler(Sampler):
-    def __init__(self, data_source, class_idx):
-        self.data_source = data_source
-        self.class_idx = class_idx
-        nb_examples = 0
-        mylist = []
-        for i, (data, label) in enumerate(self.data_source):
-            if label == self.class_idx:
-                nb_examples += 1
-                mylist.append(i)
-        self.mylist = mylist
-        self.nb_examples = nb_examples
-    
-    def __iter__(self):
-        return iter(self.mylist)
-    
-    def __len__(self):
-        return self.nb_examples
-
-
-def equal_split_func(incorr_idx, corr_idx, seed=None):
-    if seed is not None:
-        np.random.seed(seed)
-    else:
-        print("Warning: No seed provided for equal_split")
-    if len(incorr_idx) < len(corr_idx):
-        corr_idx  = np.random.choice(corr_idx, len(incorr_idx), replace=False)
-    elif len(incorr_idx) > len(corr_idx):
-        # print(f"Warning: More incorrect examples than correct ones for class {class_idx}")
-        incorr_idx  = np.random.choice(incorr_idx, len(corr_idx), replace=False)
-    return incorr_idx, corr_idx
-
-
-def class_split_func(results, incorr_idx, corr_idx, class_idx):
-    candidates = np.where(results[0, :, 1] == class_idx)[0]
-    incorr_idx = np.intersect1d(candidates, incorr_idx)
-    corr_idx = np.intersect1d(candidates, corr_idx)
-    return incorr_idx, corr_idx
-
-
-def get_corr_idx(result_path, equal_split, seed=None, class_idx=None, equal_split_first=False):
-    results = np.load(result_path, mmap_mode="r")
-    test_angles = np.sort(np.unique(results[:,:,-1]))
-    idx0 = np.where(test_angles==0)[0][0]
-    # columns = ["idx", "label", "prediction", "confidence", "angle"]
-    incorr_idx = np.where(results[idx0,:,1] != results[idx0,:,2])[0]
-    corr_idx = np.where(results[idx0,:,1] == results[idx0,:,2])[0]
-
-    if equal_split_first:
-        if equal_split:
-            incorr_idx, corr_idx = equal_split_func(incorr_idx, corr_idx, seed)
-        if class_idx is not None:
-            incorr_idx, corr_idx = class_split_func(results, incorr_idx, corr_idx, class_idx)
-        if equal_split:
-            incorr_idx, corr_idx = equal_split_func(incorr_idx, corr_idx, seed)
-    else:
-        if class_idx is not None:
-            incorr_idx, corr_idx = class_split_func(results, incorr_idx, corr_idx, class_idx)
-        if equal_split:
-            incorr_idx, corr_idx = equal_split_func(incorr_idx, corr_idx, seed)
-    return incorr_idx, corr_idx, test_angles
-
-
-# Used for generating images from one class only
-class incorrectSampler(Sampler):
-    def __init__(self, data_source, result_path, correct, equal_split=True, class_idx=None, seed=None):
-        self.data_source = data_source
-        self.correct = correct
-        incorr_idx, corr_idx, test_angles = get_corr_idx(result_path, equal_split, seed, class_idx)
-        self.test_angles = test_angles
-        self.idx = corr_idx if correct else incorr_idx
-
-    def __iter__(self):
-        return iter(self.idx)
-    
-    def __len__(self):
-        return len(self.idx)
