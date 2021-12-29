876751c8898261bd20ad4dd569fee3e6594f64ae
Zukang Liao <zukang.liao@eng.ox.ac.uk>
2021-12-29 12:44:04 +0000

    Add resnet

--- model.py
+++ model.py
@@ -97,4 +97,96 @@ class SimpleNet(nn.Module):
         results["Linear_1"] = F.relu(self.fc1(x))
         results["Linear_0"] = self.fc2(results["Linear_1"])
         return results
+
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(in_planes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+    
+    def forward(self, x):
+        t = self.conv1(x)
+        out = F.relu(self.bn1(t))
+        t = self.conv2(out)
+        out = self.bn2(self.conv2(out))
+        t = self.shortcut(x)
+        out += t
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = conv3x3(3,64)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+        
+        self.collecting = False
+        self.get_feature_names()
+    
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        y = self.linear(out)
+        return y
+    
+    def inspect(self, x):
+        results = {}
+        # batch_size = x.size(0)
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        results["Conv2d_3"] = out
+        out = self.layer4(out)
+        results["Conv2d_4"] = out
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        y = self.linear(out)
+        results["Linear_0"] = y
+        return results
+
+    def get_feature_names(self):
+        self.feature_names = ["Conv2d_3", "Conv2d_4", "Linear_0"]
+    
+    def load(self, path="resnet_cifar10.pth"):
+        tm = torch.load(path, map_location="cpu")        
+        self.load_state_dict(tm)
         
